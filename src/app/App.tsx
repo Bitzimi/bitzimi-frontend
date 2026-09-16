5462dd869bed6060e05cc7a284e5fc97296e73a2
Bitzimi <noreply.bitzimi@gmail.com>
2026-09-16 11:21:55 +0100

    feat: mount silent matchmaking recovery coordinator

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from "react-router";
 import { Component, type ReactNode } from "react";
 import { router } from "./routes";
 import { Toaster } from "./components/ui/sonner";
+import { MatchmakingRecovery } from "./components/MatchmakingRecovery";
 import { SettingsProvider } from "./contexts/SettingsContext";
 import { PlatformProvider } from "./contexts/PlatformContext";
 import { WalletProvider } from "./contexts/WalletContext";
@@ -53,6 +54,7 @@ export default function App() {
                 <TransactionProvider>
                   <AppLifecycle />
                   <RouterErrorBoundary>
+                    <MatchmakingRecovery />
                     <RouterProvider router={router} />
                   </RouterErrorBoundary>
                   <Toaster />
